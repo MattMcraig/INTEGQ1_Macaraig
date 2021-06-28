0574866123e07a35321479618b80e46ddf46c362
MattMcraig <you@example.com>
2021-06-28 00:13:23 -0700

    commit

---
+++ Gnarly.dl/sqlData.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Gnarly.dl;
+
+namespace Gnarly.dl
+{
+    class sqlData
+    {
+        static string connectionString = "Data Source=DESKTOP-36N9A4V;Initial Catalog = Gnarly; Integrated Security = True";
+        static SqlConnection sqlConnection = new SqlConnection(connectionString);
+    }
+}
